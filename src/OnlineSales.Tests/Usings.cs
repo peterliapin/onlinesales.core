ab12e80690b215e2818344287d3ea90eff33aab2
peterliapin <Peter.Liapin@waveaccess.global>
2022-10-17 00:21:34 +0530

    xUnit based test project added

--- src/OnlineSales.Tests/Usings.cs
+++ src/OnlineSales.Tests/Usings.cs
@@ -1,1 +1,5 @@
-﻿
+﻿// <copyright file="Usings.cs" company="WavePoint Co. Ltd.">
+// Licensed under the MIT license. See LICENSE file in the samples root for full license information.
+// </copyright>
+
+global using Xunit;
