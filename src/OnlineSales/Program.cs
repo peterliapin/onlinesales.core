dda070e9c9fbb48bbee066a344034893ca401129
peterliapin <Peter.Liapin@waveaccess.global>
2022-10-19 22:25:33 +0530

    Added support for env variables

--- src/OnlineSales/Program.cs
+++ src/OnlineSales/Program.cs
@@ -18,6 +18,7 @@ public class Program
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
+        builder.Configuration.AddEnvironmentVariables();
 
         var servicesConfig = builder.Configuration.Get<AppSettings>();
 
