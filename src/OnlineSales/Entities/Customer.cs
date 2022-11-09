a457d5cdbc85a0b85620b89e2ccbbfe0563d9d92
Sandaruwan Vithanage <Sandaruwan.Vithanage@waveaccess.global>
2022-11-09 18:41:14 +0530

    Addition of Customer Context

---
+++ src/OnlineSales/Entities/Customer.cs
@@ -0,0 +1,37 @@
+﻿// <copyright file="Customer.cs" company="WavePoint Co. Ltd.">
+// Licensed under the MIT license. See LICENSE file in the samples root for full license information.
+// </copyright>
+
+using System.ComponentModel.DataAnnotations;
+
+namespace OnlineSales.Entities
+{
+    public class Customer : BaseEntity
+    {
+        public string LastName { get; set; } = string.Empty;
+
+        [Required]
+        public string FirstName { get; set; } = string.Empty;
+
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        public string CompanyName { get; set; } = string.Empty;
+
+        public string Address1 { get; set; } = string.Empty;
+
+        public string Address2 { get; set; } = string.Empty;
+
+        public string State { get; set; } = string.Empty;
+
+        public string Zip { get; set; } = string.Empty;
+
+        public string Location { get; set; } = string.Empty;
+
+        public string Phone { get; set; } = string.Empty;
+
+       // public TimeZoneInfo Timezone { get; set; } = default!;
+
+        public string Culture { get; set; } = string.Empty;
+    }
+}
---
+++ src/OnlineSales/Migrations/20221109130916_Customer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace OnlineSales.Migrations
+{
+    public partial class Customer : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "customers",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    last_name = table.Column<string>(type: "text", nullable: false),
+                    first_name = table.Column<string>(type: "text", nullable: false),
+                    email = table.Column<string>(type: "text", nullable: false),
+                    company_name = table.Column<string>(type: "text", nullable: false),
+                    address1 = table.Column<string>(type: "text", nullable: false),
+                    address2 = table.Column<string>(type: "text", nullable: false),
+                    state = table.Column<string>(type: "text", nullable: false),
+                    zip = table.Column<string>(type: "text", nullable: false),
+                    location = table.Column<string>(type: "text", nullable: false),
+                    phone = table.Column<string>(type: "text", nullable: false),
+                    culture = table.Column<string>(type: "text", nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    created_by_ip = table.Column<string>(type: "text", nullable: true),
+                    created_by_user_agent = table.Column<string>(type: "text", nullable: true),
+                    updated_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    updated_by_ip = table.Column<string>(type: "text", nullable: true),
+                    updated_by_user_agent = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_customers", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "customers");
+        }
+    }
+}
