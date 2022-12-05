849dfb9482af2ce3f18ca5629ab077af5ceed48f
peterliapin <Peter.Liapin@waveaccess.global>
2022-12-05 09:43:04 +0530

    More logic added to Vsto plugin boilerplate

--- plugins/OnlineSales.Plugin.Vsto/VstoFileInfo.cs
+++ plugins/OnlineSales.Plugin.Vsto/VstoFileInfo.cs
@@ -2,12 +2,50 @@
 // Licensed under the MIT license. See LICENSE file in the samples root for full license information.
 // </copyright>
 
+using Microsoft.Extensions.FileProviders;
+
 namespace OnlineSales.Plugin.Vsto;
 
-public class VstoFileInfo
+public class VstoFileInfo : IFileInfo
 {
-    public VstoFileInfo()
+    private readonly string filePath;
+    private readonly string basePath;
+
+    public VstoFileInfo(string basePath, string filePath)
     {
+        this.basePath = basePath;
+        this.filePath = filePath;        
+    }
+
+    public string FullPath => Path.Join(basePath, filePath);
+
+    public bool Exists => File.Exists(FullPath);
+
+    public bool IsDirectory => false;
+
+    public DateTimeOffset LastModified
+    {
+        get
+        {
+            return File.GetLastWriteTime(FullPath);
+        }
+    }
+
+    public long Length
+    {
+        get
+        {
+            return File.ReadAllBytes(FullPath).Length;
+        }
+    }
+
+    public string Name => Path.GetFileName(FullPath);
+
+    public string PhysicalPath => string.Empty;
+
+    public Stream CreateReadStream()
+    {
+        return File.OpenRead(FullPath);
     }
 }
 
--- plugins/OnlineSales.Plugin.Vsto/VstoPlugin.cs
+++ plugins/OnlineSales.Plugin.Vsto/VstoPlugin.cs
@@ -2,20 +2,39 @@
 // Licensed under the MIT license. See LICENSE file in the samples root for full license information.
 // </copyright>
 
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using OnlineSales.Infrastructure;
+using OnlineSales.Plugin.Vsto.Configuration;
 
 namespace OnlineSales.Plugin.Vsto;
 
-public class VstoPlugin : IPlugin
+public class VstoPlugin : IPlugin, IPluginApplication
 {
-    public VstoPlugin()
+    public static PluginConfig Configuration { get; private set; } = new PluginConfig();
+
+    public void ConfigureServices(IServiceCollection services, IConfiguration configuration)
     {
+        var pluginConfig = configuration.Get<PluginConfig>();
+
+        if (pluginConfig != null)
+        {
+            Configuration = pluginConfig;
+        }
     }
 
-    public void Configure(IServiceCollection services, IConfiguration configuration)
+    public void ConfigureApplication(IApplicationBuilder application)
     {
-        // nothing   
+        /* var httpContextHelper = application.ApplicationServices.GetRequiredService<HttpContextHelper>();
+
+        application.UseStaticFiles(new StaticFileOptions
+        {
+            RequestPath = Configuration.Vsto.RequestPath,
+            FileProvider = new VstoFileProvider(httpContextHelper),
+            ServeUnknownFileTypes = true,
+        });*/
     }
 }
 
