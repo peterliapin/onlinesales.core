03b38db67067d7f8141d4175c37923609060b239
Boris Alexandrov <Boris.Alexandrov@waveaccess.global>
2022-12-08 22:55:38 +0300

    Added customers controller

---
+++ src/OnlineSales/Controllers/CustomersController.cs
@@ -0,0 +1,21 @@
+﻿// <copyright file="CustomersController.cs" company="WavePoint Co. Ltd.">
+// Licensed under the MIT license. See LICENSE file in the samples root for full license information.
+// </copyright>
+
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using OnlineSales.Data;
+using OnlineSales.DTOs;
+using OnlineSales.Entities;
+
+namespace OnlineSales.Controllers;
+
+[Route("api/[controller]")]
+public class CustomersController : BaseController<Customer, CustomerCreateDto, CustomerUpdateDto>
+{
+    public CustomersController(ApiDbContext dbContext, IMapper mapper)
+        : base(dbContext, mapper)
+    {
+    }
+}
+
---
+++ src/OnlineSales/DTOs/CustomerDtos.cs
@@ -0,0 +1,73 @@
+﻿// <copyright file="CustomerDtos.cs" company="WavePoint Co. Ltd.">
+// Licensed under the MIT license. See LICENSE file in the samples root for full license information.
+// </copyright>
+
+using System.ComponentModel.DataAnnotations;
+
+namespace OnlineSales.DTOs;
+
+public class CustomerCreateDto
+{
+    [Required]
+    public string? LastName { get; set; }
+
+    [Required]
+    public string? FirstName { get; set; }
+
+    [EmailAddress]
+    public string Email { get; set; } = string.Empty;
+
+    [Required]
+    public string? CompanyName { get; set; }
+
+    public string? Address1 { get; set; }
+
+    public string? Address2 { get; set; }
+
+    public string? State { get; set; }
+
+    public string? Zip { get; set; }
+
+    public string? Location { get; set; }
+
+    [Required]
+    public string? Phone { get; set; }
+
+    [Required]
+    public int? Timezone { get; set; }
+
+    public string? Culture { get; set; }
+}
+
+public class CustomerUpdateDto
+{
+    [Required]
+    public string? LastName { get; set; }
+
+    [Required]
+    public string? FirstName { get; set; }
+
+    [EmailAddress]
+    public string Email { get; set; } = string.Empty;
+
+    [Required]
+    public string? CompanyName { get; set; }
+
+    public string? Address1 { get; set; }
+
+    public string? Address2 { get; set; }
+
+    public string? State { get; set; }
+
+    public string? Zip { get; set; }
+
+    public string? Location { get; set; }
+
+    [Required]
+    public string? Phone { get; set; }
+
+    [Required]
+    public int? Timezone { get; set; }
+
+    public string? Culture { get; set; }
+}
