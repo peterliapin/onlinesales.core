197416de06f5f05dcf22a10a04bd50542aed25d6
Igor Kostyukov <ikostyukov@bkmedical.com>
2022-12-08 14:58:03 +0300

    EmailGroupTests

---
+++ tests/OnlineSales.Tests/EmailGroupsTests.cs
@@ -0,0 +1,84 @@
+﻿// <copyright file="EmailGroupsTests.cs" company="WavePoint Co. Ltd.">
+// Licensed under the MIT license. See LICENSE file in the samples root for full license information.
+// </copyright>
+
+using FluentAssertions;
+using Namotion.Reflection;
+using OnlineSales.DTOs;
+using OnlineSales.Entities;
+
+namespace OnlineSales.Tests;
+
+public class EmailGroupsTests : BaseTest
+{
+    private static readonly string UrlEmailGroups = "/api/emailgroups";
+    private static readonly string UrlEmailGroupsNotFound = UrlEmailGroups + "/404";
+    private static readonly string EmailGroupName = "EmailGroupName";
+    private static readonly string EmailGroupUpdatedName = "EmailGroupUpdatedName";
+
+    [Fact]
+    public async Task GetEmailGroupNotFoundTest()
+    {
+        await GetTest(UrlEmailGroupsNotFound, HttpStatusCode.NotFound);
+    }
+
+    [Fact]
+    public async Task CreateAndGetEmailGroupTest()
+    {
+        var testEmailGroup = new EmailGroupCreateDto();
+
+        testEmailGroup.Name = EmailGroupName;
+
+        var newEmailGroupUrl = await PostTest(UrlEmailGroups, testEmailGroup);
+
+        var emailGroup = await GetTest<EmailGroup>(newEmailGroupUrl);
+
+        emailGroup.Should().BeEquivalentTo(testEmailGroup);
+    }
+
+    [Fact]
+    public async Task UpdateEmailGroupNotFoundTest()
+    {
+        await PatchTest(UrlEmailGroupsNotFound, new { }, HttpStatusCode.NotFound);
+    }
+
+    [Fact]
+    public async Task CreateAndUpdateEmailGroupNameTest()
+    {
+        var testEmailGroup = new EmailGroupCreateDto();
+
+        testEmailGroup.Name = EmailGroupName;
+
+        var newEmailGroupUrl = await PostTest(UrlEmailGroups, testEmailGroup);
+
+        var updatedEmailGroup = new EmailGroupUpdateDto();
+
+        testEmailGroup.Name = updatedEmailGroup.Name = EmailGroupUpdatedName;
+
+        await PatchTest(newEmailGroupUrl, updatedEmailGroup);
+
+        var emailGroup = await GetTest<EmailTemplate>(newEmailGroupUrl);
+
+        emailGroup.Should().BeEquivalentTo(testEmailGroup);
+    }
+
+    [Fact]
+    public async Task DeleteEmailGroupNotFoundTest()
+    {
+        await DeleteTest(UrlEmailGroupsNotFound, HttpStatusCode.NotFound);
+    }
+
+    [Fact]
+    public async Task CreateAndDeleteEmailGroupTest()
+    {
+        var testEmailGroup = new EmailGroupCreateDto();
+
+        testEmailGroup.Name = EmailGroupName;
+
+        var newEmailGroupUrl = await PostTest(UrlEmailGroups, testEmailGroup);
+
+        await DeleteTest(newEmailGroupUrl);
+
+        await GetTest(newEmailGroupUrl, HttpStatusCode.NotFound);
+    }
+}
