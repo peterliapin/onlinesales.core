4730526ece21ca2d983bbdead1e04ce16991f649
Igor Kostyukov <ikostyukov@bkmedical.com>
2022-12-09 12:46:21 +0300

    Customer controller and dtos

---
+++ src/OnlineSales/DTOs/CustomerDtos.cs
@@ -0,0 +1,61 @@
+﻿// <copyright file="CustomerDtos.cs" company="WavePoint Co. Ltd.">
+// Licensed under the MIT license. See LICENSE file in the samples root for full license information.
+// </copyright>
+
+using System.ComponentModel.DataAnnotations;
+
+namespace OnlineSales.DTOs;
+public class CustomerCreateDto
+{
+    public string? LastName { get; set; }
+
+    public string? FirstName { get; set; }
+
+    [Required]
+    public string Email { get; set; } = string.Empty;
+
+    public string? CompanyName { get; set; }
+
+    public string? Address1 { get; set; }
+
+    public string? Address2 { get; set; }
+
+    public string? State { get; set; }
+
+    public string? Zip { get; set; }
+
+    public string? Location { get; set; }
+
+    public string? Phone { get; set; }
+
+    public int? Timezone { get; set; }
+
+    public string? Culture { get; set; }
+}
+
+public class CustomerUpdateDto
+{
+    public string? LastName { get; set; }
+
+    public string? FirstName { get; set; }
+
+    public string? Email { get; set; }
+
+    public string? CompanyName { get; set; }
+
+    public string? Address1 { get; set; }
+
+    public string? Address2 { get; set; }
+
+    public string? State { get; set; }
+
+    public string? Zip { get; set; }
+
+    public string? Location { get; set; }
+
+    public string? Phone { get; set; }
+
+    public int? Timezone { get; set; }
+
+    public string? Culture { get; set; }
+}
