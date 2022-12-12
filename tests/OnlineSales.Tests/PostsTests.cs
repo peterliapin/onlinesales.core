def5eda64f81a1f81a1819264e78bc80f5e4d367
Igor Kostyukov <ikostyukov@bkmedical.com>
2022-12-12 14:02:42 +0300

    PostsTests

---
+++ tests/OnlineSales.Tests/PostsTests.cs
@@ -0,0 +1,18 @@
+﻿// <copyright file="PostsTests.cs" company="WavePoint Co. Ltd.">
+// Licensed under the MIT license. See LICENSE file in the samples root for full license information.
+// </copyright>
+
+using FluentAssertions;
+using OnlineSales.DTOs;
+using OnlineSales.Entities;
+
+namespace OnlineSales.Tests;
+
+public class PostsTests : SimpleTableTests<Post, TestPostCreateDto, TestPostUpdateDto, TestPostConverter>
+{
+    public PostsTests()
+        : base("/api/posts")
+    {
+    }    
+}
+
--- tests/OnlineSales.Tests/TestEntities/TestPost.cs
+++ tests/OnlineSales.Tests/TestEntities/TestPost.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT license. See LICENSE file in the samples root for full license information.
 // </copyright>
 
+using Microsoft.AspNetCore.Mvc.Formatters;
 using OnlineSales.DTOs;
 
 namespace OnlineSales.Tests.TestEntities;
@@ -22,5 +23,43 @@ public class TestPostCreateDto : PostCreateDto
         Categories = "This is Categories";
         Tags = "Sample Tags";
         AllowComments = true;
+    }
+}
+
+public class TestPostUpdateDto : PostUpdateDto
+{
+    public TestPostUpdateDto()
+    {
+        Slug = "Test Slug " + new Random().Next(0, 50000).ToString() + " - " + DateTime.Now.Year.ToString() + "-" + DateTime.Now.Month.ToString() + "-" + DateTime.Now.Day.ToString();
+        Template = "Test Template Update";
+        Author = "Peter Liapin Update";
+        Title = "Test Title (via test suit) Update";
+        Description = "This is a sample test description Update";
+        Content = "This is a sample Content Update";
+        CoverImageUrl = "This is a sample Cover Update";
+        CoverImageAlt = "This is a sample Cover alt Update";
+        Language = "ru-RU";
+        Categories = "This is Categories Update";
+        Tags = "Sample Tags Update";
+        AllowComments = false;
+    }
+}
+
+public class TestPostConverter : ITestTypeConverter<TestPostUpdateDto, TestPostCreateDto>
+{
+    public void Convert(TestPostUpdateDto from, TestPostCreateDto to)
+    {
+        to.Slug = from.Slug ?? to.Slug;
+        to.Template = from.Template ?? to.Template;
+        to.Author = from.Author ?? to.Author;
+        to.Title = from.Title ?? to.Title;
+        to.Description = from.Description ?? to.Description;
+        to.Content = from.Content ?? to.Content;
+        to.CoverImageAlt = from.CoverImageAlt ?? to.CoverImageAlt;
+        to.CoverImageUrl = from.CoverImageUrl ?? to.CoverImageUrl;
+        to.Language = from.Language ?? to.Language;
+        to.Categories = from.Categories ?? to.Categories;
+        to.Tags = from.Tags ?? to.Tags;
+        to.AllowComments = from.AllowComments ?? to.AllowComments;
     }
 }
