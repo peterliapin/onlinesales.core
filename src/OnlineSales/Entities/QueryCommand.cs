7757564fcad28d1788aa9f796c086a1ae8a3b66a
Boris Alexandrov <Boris.Alexandrov@waveaccess.global>
2022-12-15 11:18:42 +0300

    Begin work on data query DSL

---
+++ src/OnlineSales/Entities/QueryCommand.cs
@@ -0,0 +1,57 @@
+﻿// <copyright file="QueryCommand.cs" company="WavePoint Co. Ltd.">
+// Licensed under the MIT license. See LICENSE file in the samples root for full license information.
+// </copyright>
+
+namespace OnlineSales.Entities
+{
+    public enum FilterType
+    {
+        Fields = 0,
+        Limit = 1,
+        Order = 2,
+        Skip = 3,
+        Where = 4,
+        None = 5,
+    }
+
+    public enum WOperand
+    {
+        Equal = 0,
+        And = 1,
+        Or = 2,
+        GreaterThan = 3,
+        GreaterThanOrEquals = 4,
+        LessThan = 5,
+        LessThanOrEquals = 6,
+        NotEqual = 7,
+    }
+
+    public class QueryCommand
+    {
+        public static Dictionary<string, FilterType> FilterMappings { get; } = new Dictionary<string, FilterType>()
+        {
+            { "field", FilterType.Fields },
+            { "limit", FilterType.Limit },
+            { "order", FilterType.Order },
+            { "skip", FilterType.Skip },
+            { "where", FilterType.Where },
+        };
+
+        public static Dictionary<string, WOperand> OperandMappings { get; } = new Dictionary<string, WOperand>()
+        {
+            { "and", WOperand.And },
+            { "or", WOperand.Or },
+            { "gt", WOperand.GreaterThan },
+            { "gte", WOperand.GreaterThanOrEquals },
+            { "lt", WOperand.LessThan },
+            { "lte", WOperand.LessThanOrEquals },
+            { "neq", WOperand.NotEqual },
+        };
+
+        public FilterType Type { get; set; } = FilterType.None;
+
+        public string[] Props { get; set; } = Array.Empty<string>();
+
+        public string Value { get; set; } = string.Empty;
+    }
+}
---
+++ src/OnlineSales/Infrastructure/TypeHelper.cs
@@ -0,0 +1,86 @@
+﻿// <copyright file="TypeHelper.cs" company="WavePoint Co. Ltd.">
+// Licensed under the MIT license. See LICENSE file in the samples root for full license information.
+// </copyright>
+
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace OnlineSales.Infrastructure
+{
+    public static class TypeHelper
+    {
+        private static Dictionary<Type[], Type> typeCache = new Dictionary<Type[], Type>();
+
+        public static Type CompileTypeForSelectStatement(PropertyInfo[] types)
+        {
+            TypeBuilder tb = GetTypeBuilder();
+            // ConstructorBuilder constructor = tb.DefineDefaultConstructor(MethodAttributes.Public | MethodAttributes.SpecialName | MethodAttributes.RTSpecialName);
+
+            // NOTE: assuming your list contains Field objects with fields FieldName(string) and FieldType(Type)
+            foreach (var field in types)
+            {
+                CreateProperty(tb, field.Name, field.PropertyType);
+            }
+
+            Type objectType = tb.CreateType();
+            return objectType;
+        }
+
+        private static TypeBuilder GetTypeBuilder()
+        {
+            var typeSignature = "MyDynamicType";
+            var an = new AssemblyName(typeSignature);
+            AssemblyBuilder assemblyBuilder = AssemblyBuilder.DefineDynamicAssembly(an, AssemblyBuilderAccess.Run);
+            ModuleBuilder moduleBuilder = assemblyBuilder.DefineDynamicModule("MainModule");
+            var typeAttributes =
+                TypeAttributes.Public |
+                TypeAttributes.Class |
+                TypeAttributes.AutoClass |
+                TypeAttributes.AnsiClass |
+                TypeAttributes.BeforeFieldInit |
+                TypeAttributes.AutoLayout;
+
+            TypeBuilder tb = moduleBuilder.DefineType(
+                typeSignature,
+                typeAttributes,
+                null);
+            return tb;
+        }
+
+        private static void CreateProperty(TypeBuilder tb, string propertyName, Type propertyType)
+        {
+            FieldBuilder fieldBuilder = tb.DefineField("_" + propertyName, propertyType, FieldAttributes.Private);
+
+            PropertyBuilder propertyBuilder = tb.DefineProperty(propertyName, PropertyAttributes.HasDefault, propertyType, null);
+            MethodBuilder getPropMthdBldr = tb.DefineMethod(propertyName, MethodAttributes.Public | MethodAttributes.SpecialName | MethodAttributes.HideBySig, propertyType, Type.EmptyTypes);
+            ILGenerator getIl = getPropMthdBldr.GetILGenerator();
+
+            getIl.Emit(OpCodes.Ldarg_0);
+            getIl.Emit(OpCodes.Ldfld, fieldBuilder);
+            getIl.Emit(OpCodes.Ret);
+
+            var methodName = propertyName;
+            var methodAttributes = MethodAttributes.Public |
+                  MethodAttributes.SpecialName |
+                  MethodAttributes.HideBySig;
+
+            MethodBuilder setPropMthdBldr = tb.DefineMethod(methodName, methodAttributes, null, new[] { propertyType });
+
+            ILGenerator setIl = setPropMthdBldr.GetILGenerator();
+            Label modifyProperty = setIl.DefineLabel();
+            Label exitSet = setIl.DefineLabel();
+
+            setIl.MarkLabel(modifyProperty);
+            setIl.Emit(OpCodes.Ldarg_0);
+            setIl.Emit(OpCodes.Ldarg_1);
+            setIl.Emit(OpCodes.Stfld, fieldBuilder);
+
+            setIl.Emit(OpCodes.Nop);
+            setIl.MarkLabel(exitSet);
+            setIl.Emit(OpCodes.Ret);
+
+            propertyBuilder.SetGetMethod(getPropMthdBldr);
+            propertyBuilder.SetSetMethod(setPropMthdBldr);
+        }
+    }
+}
