49147ffd4fb2b10cea3313dfaf01768cbec40938
peterliapin <Peter.Liapin@waveaccess.global>
2022-12-20 00:16:20 +0530

    The continent and the country enums added

---
+++ src/OnlineSales/Geography/Continent.cs
@@ -0,0 +1,28 @@
+﻿// <copyright file="Continent.cs" company="WavePoint Co. Ltd.">
+// Licensed under the MIT license. See LICENSE file in the samples root for full license information.
+// </copyright>
+
+using System.ComponentModel;
+
+namespace OnlineSales.Geography;
+
+public enum Continent
+{
+    [Description("Unknown")]
+    ZZ = 0,
+
+    [Description("Africa")]
+    AF = 1,
+    [Description("Antarctica")]
+    AN = 2,
+    [Description("Asia")]
+    AS = 3,
+    [Description("Europe")]
+    EU = 4,
+    [Description("North America")]
+    NA = 5,
+    [Description("Oceania")]
+    OC = 6,
+    [Description("South America")]
+    SA = 7,
+}
