7b179ca19248b22600def7a45330e3ee809c4fff
Lakmal Priyadarshana <Lakmal.Priyadarshana@waveaccess.global>
2022-12-19 14:27:46 +0530

    Implementation of variable service and variable provider interface

---
+++ src/OnlineSales/Interfaces/IVariableProvider.cs
@@ -0,0 +1,16 @@
+﻿// <copyright file="IVariableProvider.cs" company="WavePoint Co. Ltd.">
+// Licensed under the MIT license. See LICENSE file in the samples root for full license information.
+// </copyright>
+
+namespace OnlineSales.Interfaces;
+
+public interface IVariableProvider
+{
+    public Dictionary<string, string> Variables { get; set; }
+
+    public string Key { get; set; }
+
+    public string Language { get; set; }
+
+    public void GenerateVariables();
+}
