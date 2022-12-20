25dd6eeb3ac6bb56d51e980ee47e0f2d6a974600
Sandaruwan Vithanage <Sandaruwan.Vithanage@waveaccess.global>
2022-12-20 16:39:12 +0530

    Addition Change Log to the Project

---
+++ src/OnlineSales/Entities/ChangeLog.cs
@@ -0,0 +1,30 @@
+﻿// <copyright file="ChangeLog.cs" company="WavePoint Co. Ltd.">
+// Licensed under the MIT license. See LICENSE file in the samples root for full license information.
+// </copyright>
+
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OnlineSales.Entities
+{
+    public enum Operation
+    {
+        NA = 0,
+        Inserted = 1,
+        Updated = 2,
+        Deleted = 3,
+    }
+
+    [Table("change_log")]
+    public class ChangeLog : BaseEntityWithId
+    {
+        public int ObjectType { get; set; }
+
+        public int ObjectId { get; set; }
+
+        public Operation Operation { get; set; } = Operation.NA;
+
+        [Column(TypeName = "jsonb")]
+        public string Data { get; set; } = string.Empty;
+    }
+}
+
---
+++ src/OnlineSales/Migrations/20221220110844_ChangeLogAddition.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace OnlineSales.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeLogAddition : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "change_log",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    objecttype = table.Column<int>(name: "object_type", type: "integer", nullable: false),
+                    objectid = table.Column<int>(name: "object_id", type: "integer", nullable: false),
+                    operation = table.Column<int>(type: "integer", nullable: false),
+                    data = table.Column<string>(type: "jsonb", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_change_log", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "change_log");
+        }
+    }
+}
