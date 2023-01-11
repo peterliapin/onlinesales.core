79d912c3d16bb70a0ae1f0aa1b8a54fa8fc9b534
Lakmal Priyadarshana <Lakmal.Priyadarshana@waveaccess.global>
2023-01-11 16:44:27 +0530

    Reverted test related changes

--- src/OnlineSales/Controllers/OrderItemsController.cs
+++ src/OnlineSales/Controllers/OrderItemsController.cs
@@ -16,7 +16,7 @@ using OnlineSales.Interfaces;
 
 namespace OnlineSales.Controllers
 {
-    // [Authorize]
+    [Authorize]
     [Route("api/[controller]")]
     public class OrderItemsController : BaseControllerWithImport<OrderItem, OrderItemCreateDto, OrderItemUpdateDto, OrderItemDetailsDto, OrderItemImportDto>
     {
