47d1aa03ca9eaa1e2bbc7c9afb33ac054ec02dac
peterliapin <Peter.Liapin@waveaccess.global>
2023-01-16 02:10:27 +0530

    IpDetailsException rename

--- src/OnlineSales/Exceptions/IpDetailsException.cs
+++ src/OnlineSales/Exceptions/IpDetailsException.cs
@@ -2,29 +2,12 @@
 // Licensed under the MIT license. See LICENSE file in the samples root for full license information.
 // </copyright>
 
-using System.Runtime.Serialization;
-
 namespace OnlineSales.Exceptions
 {
-    [Serializable]
     public class IpDetailsException : Exception
     {
-        public IpDetailsException()
-        {
-        }
-
         public IpDetailsException(string? message)
             : base(message)
-        {
-        }
-
-        public IpDetailsException(string? message, Exception? innerException)
-            : base(message, innerException)
-        {
-        }
-
-        protected IpDetailsException(SerializationInfo info, StreamingContext context)
-            : base(info, context)
         {
         }
     }
