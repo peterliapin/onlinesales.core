fe4310ef2f0877f5cef285e51fdfd9688d9e5d79
peterliapin <Peter.Liapin@waveaccess.global>
2023-01-16 02:09:44 +0530

    IpDetailsException rename

---
+++ src/OnlineSales/Exceptions/IPDetailsException22.cs
@@ -0,0 +1,15 @@
+﻿
+// <copyright file="IpDetailsException.cs" company="WavePoint Co. Ltd.">
+// Licensed under the MIT license. See LICENSE file in the samples root for full license information.
+// </copyright>
+
+namespace OnlineSales.Exceptions
+{
+    public class IpDetailsException : Exception
+    {
+        public IpDetailsException(string? message)
+            : base(message)
+        {
+        }
+    }
+}
