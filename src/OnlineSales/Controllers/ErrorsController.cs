56278ea414c7ed6c02f22c71d1432da1cc0f9038
UniqueUlysees <boriscool007@gmail.com>
2023-01-18 11:32:08 +0300

    Removed redundant using

--- src/OnlineSales/Controllers/ErrorsController.cs
+++ src/OnlineSales/Controllers/ErrorsController.cs
@@ -2,7 +2,6 @@
 // Licensed under the MIT license. See LICENSE file in the samples root for full license information.
 // </copyright>
 
-using System.Net.Quic;
 using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
