8130ab4b25c45c2fe4ed886d3499d59ec246cb02
UniqueUlysees <boriscool007@gmail.com>
2023-02-27 17:54:48 +0300

    Added YARP plugin

---
+++ plugins/OnlineSales.Plugin.ReverseProxy/ReverseProxyPlugin.cs
@@ -0,0 +1,52 @@
+﻿// <copyright file="ReverseProxyPlugin.cs" company="WavePoint Co. Ltd.">
+// Licensed under the MIT license. See LICENSE file in the samples root for full license information.
+// </copyright>
+
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Identity.Web;
+using Microsoft.Identity.Web.UI;
+using OnlineSales.Exceptions;
+using OnlineSales.Interfaces;
+
+namespace OnlineSales.Plugin.ReverseProxy;
+
+public class AzureADPlugin : IPlugin, IPluginApplication
+{
+    public void ConfigureServices(IServiceCollection services, IConfiguration configuration)
+    {
+        var proxyConfig = configuration.GetSection("YARPSettings");
+        if (proxyConfig == null)
+        {
+            throw new MissingConfigurationException("YARP configuration is mandatory.");
+        }
+
+        services.AddAuthorization(options =>
+        {
+            options.AddPolicy("AuthPolicy", policy =>
+                policy.RequireAuthenticatedUser());
+        });
+
+        services.AddReverseProxy().LoadFromConfig(proxyConfig);
+        services.AddMicrosoftIdentityWebAppAuthentication(configuration, "AzureAd");
+
+        services.AddRazorPages().AddMvcOptions(options =>
+        {
+            var policy = new AuthorizationPolicyBuilder()
+                .RequireAuthenticatedUser()
+                .Build();
+            options.Filters.Add(new AuthorizeFilter(policy));
+        }).AddMicrosoftIdentityUI();
+    }
+
+    public void ConfigureApplication(IApplicationBuilder application)
+    {
+        var app = (WebApplication)application;
+        app.MapRazorPages();
+        app.MapReverseProxy();
+    }
+}
