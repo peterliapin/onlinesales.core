d0fae9fcd3870a2dc015a885c615957898abd850
UniqueUlysees <boriscool007@gmail.com>
2023-04-26 18:09:25 +0300

    Added lic header for IdentityHelper

--- src/OnlineSales/Infrastructure/IdentityHelper.cs
+++ src/OnlineSales/Infrastructure/IdentityHelper.cs
@@ -1,4 +1,7 @@
-﻿using System.Security.Claims;
+﻿// <copyright file="IdentityHelper.cs" company="WavePoint Co. Ltd.">
+// Licensed under the MIT license. See LICENSE file in the samples root for full license information.
+// </copyright>
+using System.Security.Claims;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Identity.Web;
