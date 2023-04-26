af6a10a70d1ece1fe7afe233dd447f7553846453
UniqueUlysees <boriscool007@gmail.com>
2023-04-26 10:29:00 +0300

    Renamed tables to match conventions

--- src/OnlineSales/Data/PgDbContext.cs
+++ src/OnlineSales/Data/PgDbContext.cs
@@ -213,13 +213,13 @@ public class PgDbContext : IdentityDbContext<User>
         base.OnModelCreating(builder);
 
         // Override default AspNet Identity table names
-        builder.Entity<User>(entity => { entity.ToTable(name: "Users"); });
-        builder.Entity<IdentityRole>(entity => { entity.ToTable(name: "Roles"); });
-        builder.Entity<IdentityUserRole<string>>(entity => { entity.ToTable("UserRoles"); });
-        builder.Entity<IdentityUserClaim<string>>(entity => { entity.ToTable("UserClaims"); });
-        builder.Entity<IdentityUserLogin<string>>(entity => { entity.ToTable("UserLogins"); });
-        builder.Entity<IdentityUserToken<string>>(entity => { entity.ToTable("UserTokens"); });
-        builder.Entity<IdentityRoleClaim<string>>(entity => { entity.ToTable("RoleClaims"); });
+        builder.Entity<User>(entity => { entity.ToTable(name: "users"); });
+        builder.Entity<IdentityRole>(entity => { entity.ToTable(name: "roles"); });
+        builder.Entity<IdentityUserRole<string>>(entity => { entity.ToTable("user_roles"); });
+        builder.Entity<IdentityUserClaim<string>>(entity => { entity.ToTable("user_claims"); });
+        builder.Entity<IdentityUserLogin<string>>(entity => { entity.ToTable("user_logins"); });
+        builder.Entity<IdentityUserToken<string>>(entity => { entity.ToTable("user_tokens"); });
+        builder.Entity<IdentityRoleClaim<string>>(entity => { entity.ToTable("role_claims"); });
     }
 
     private DateTime GetDateWithKind(DateTime date)
