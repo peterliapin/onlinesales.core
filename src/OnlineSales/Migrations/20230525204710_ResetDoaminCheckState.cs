96d8dfaaeda5ff07bddd24180b281b34b1ec624c
UniqueUlysees <boriscool007@gmail.com>
2023-05-25 23:55:23 +0300

    Optimized domain check method. Fixed HTML decoding method. Fixed redirection issue

---
+++ src/OnlineSales/Migrations/20230525204710_ResetDoaminCheckState.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OnlineSales.Migrations
+{
+    /// <inheritdoc />
+    public partial class ResetDoaminCheckState : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("UPDATE  domain SET dns_check = null, http_check = null, mx_check = null");
+        }
+    }
+}
