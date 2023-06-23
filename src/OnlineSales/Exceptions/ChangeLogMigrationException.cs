374c5db2de1185e97772f03c6da1a780a4f00d3f
Igor Kostyukov <igor.kostyukov@waveaccess.global>
2023-06-23 10:43:45 +0300

    change_log migrations, part 1

---
+++ src/OnlineSales/Exceptions/ChangeLogMigrationException.cs
@@ -0,0 +1,12 @@
+﻿// <copyright file="ChangeLogMigrationException.cs" company="WavePoint Co. Ltd.">
+// Licensed under the MIT license. See LICENSE file in the samples root for full license information.
+// </copyright>
+
+namespace OnlineSales.Exceptions;
+public class ChangeLogMigrationException : Exception
+{
+    public ChangeLogMigrationException(string message)
+        : base(message)
+    {
+    }
+}
