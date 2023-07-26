7a2a3803fb4c2894d3dde737cd31e6791ac3da88
Max Vinnikov <mixer57@gmail.com>
2023-07-26 11:25:47 +0600

    Added GetProjectVersionsList into IVariablesProvider/Service

--- plugins/OnlineSales.Plugin.Vsto/VstoVariablesProvider.cs
+++ plugins/OnlineSales.Plugin.Vsto/VstoVariablesProvider.cs
@@ -2,6 +2,8 @@
 // Licensed under the MIT license. See LICENSE file in the samples root for full license information.
 // </copyright>
 
+using Microsoft.AspNetCore.Mvc;
+
 namespace OnlineSales.Plugin.Vsto;
 
 public class VstoVariablesProvider : IVariablesProvider
@@ -21,5 +23,24 @@ public class VstoVariablesProvider : IVariablesProvider
         }
 
         return variables;
+    }
+
+    public string[] GetProjectVersionsList(string project, string version, string language)
+    {
+        var ver = version.Replace(".", "_");
+
+        var dirs = Directory.GetDirectories(VstoPlugin.VstoLocalPath, $"{project}_{ver}*", SearchOption.AllDirectories);
+        if (dirs.Length == 0)
+        {
+            return Array.Empty<string>();
+        }
+
+        // filter by language
+        dirs = dirs.Where(d => d.Contains($"/{language}/") || d.Contains($"\\{language}\\")).ToArray()!;
+
+        // select versions from full directory path
+        dirs = dirs.Select(d => Path.GetFileName(d)[project.Length..].Trim('_')).ToArray();
+
+        return dirs;
     }
 }
