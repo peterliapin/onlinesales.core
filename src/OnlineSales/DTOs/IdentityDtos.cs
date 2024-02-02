05acaa8f9e2e165b296f826e6ee04154fbec134e
seregoroff <snegoroff@gmail.com>
2024-02-02 16:11:52 +0530

    IdentityController.Login

---
+++ src/OnlineSales/DTOs/IdentityDtos.cs
@@ -0,0 +1,17 @@
+﻿// <copyright file="IdentityDtos.cs" company="WavePoint Co. Ltd.">
+// Licensed under the MIT license. See LICENSE file in the samples root for full license information.
+// </copyright>
+
+using System.ComponentModel.DataAnnotations;
+using CsvHelper.Configuration.Attributes;
+
+namespace OnlineSales.DTOs;
+
+public class LoginDto
+{
+    [Required]
+    required public string Email { get; set; }
+
+    [Optional]
+    public string Password { get; set; } = string.Empty;
+}
