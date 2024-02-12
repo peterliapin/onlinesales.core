12767fcf4efb365309778ae0f3e73b3c0ff2fb22
Peter Liapin <peter@liapin.space>
2024-02-12 15:41:19 +0530

    Version 1.2.20-pre - emails scheduling improved + fixes (#244)
    * Fix of the problem with multiple email being sent in case contact was registered long time ago but did not get any emails before
    
    * Version 1.2.20-pre - emails scheduling improved
    
    ---------
    
    Co-authored-by: peterliapin <Peter.Liapin@waveaccess.global>

---
+++ src/OnlineSales/Interfaces/IEmailSchedulingService.cs
@@ -0,0 +1,16 @@
+﻿// <copyright file="IEmailSchedulingService.cs" company="WavePoint Co. Ltd.">
+// Licensed under the MIT license. See LICENSE file in the samples root for full license information.
+// </copyright>
+
+using OnlineSales.Data;
+using OnlineSales.Entities;
+
+namespace OnlineSales.Interfaces;
+
+public interface IEmailSchedulingService
+{
+    Task<EmailSchedule?> FindByGroupAndLanguage(string groupName, string languageCode);
+
+    void SetDBContext(PgDbContext pgDbContext);
+}
+
---
+++ src/OnlineSales/Services/EmailSchedulingService.cs
@@ -0,0 +1,68 @@
+﻿// <copyright file="EmailSchedulingService.cs" company="WavePoint Co. Ltd.">
+// Licensed under the MIT license. See LICENSE file in the samples root for full license information.
+// </copyright>
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using OnlineSales.Configuration;
+using OnlineSales.Data;
+using OnlineSales.Entities;
+using OnlineSales.Interfaces;
+
+namespace OnlineSales.Services;
+
+public class EmailSchedulingService : IEmailSchedulingService
+{
+    private readonly IOptions<ApiSettingsConfig> apiSettingsConfig;
+
+    private PgDbContext dbContext;
+
+    public EmailSchedulingService(PgDbContext dbContext, IOptions<ApiSettingsConfig> apiSettingsConfig)
+    {
+        this.dbContext = dbContext;
+        this.apiSettingsConfig = apiSettingsConfig;
+    }
+
+    public async Task<EmailSchedule?> FindByGroupAndLanguage(string groupName, string languageCode)
+    {
+        EmailSchedule? result;
+
+        // Check if contact.Language is in two-letter format and adjust query accordingly
+        var emailSchedulesQuery = dbContext.EmailSchedules!
+            .Include(c => c.Group)
+            .Where(e => e.Group!.Name == groupName);
+
+        if (languageCode.Length == 2)
+        {
+            result = await emailSchedulesQuery.Where(
+                            e => e.Group!.Language.StartsWith(languageCode)).FirstOrDefaultAsync();
+        }
+        else
+        {
+            result = await emailSchedulesQuery.Where(
+                            e => e.Group!.Language == languageCode).FirstOrDefaultAsync();
+
+            if (result == null)
+            {
+                var lang = languageCode.Split('-')[0];
+
+                result = await emailSchedulesQuery.Where(
+                                e => e.Group!.Language.StartsWith(lang)).FirstOrDefaultAsync();
+            }
+        }
+
+        if (result == null)
+        {
+            result = await emailSchedulesQuery.Where(
+                            e => e.Group!.Language == apiSettingsConfig.Value.DefaultLanguage).FirstOrDefaultAsync();
+        }
+
+        return result;
+    }
+
+    public void SetDBContext(PgDbContext pgDbContext)
+    {
+        dbContext = pgDbContext;
+    }
+}
+
